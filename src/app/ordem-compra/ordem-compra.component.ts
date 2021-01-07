ea8030584343a18f29d6413d36d91b9d440fb104
DANILO NOQUELI DE OLIVEIRA <danilo.n.oliveira@bradesco.com.br>
2021-01-07 10:17:21 -0300

    ajustando o template ordem compra

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
 
@@ -9,10 +10,31 @@ import { Pedido } from '../shared/pedido.model'
   providers: [ OrdemCompraService ]
 })
 export class OrdemCompraComponent implements OnInit {
+  /// Nesse caso nós estamos olhando as alterações do formulario e recuperando o formulario do HTML
+  @ViewChild("formulario") public formulario: NgForm
+
+  public idPedidoCompra: number;
 
   constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
     
+  }
+
+  public confirmaCompra(){
+    /// Os atributos pristine e dirty são estados do atributo do formulario 
+
+    // O atributo pristine é para informar que o campo não foi alterado
+
+    // O atributo dirty se o campo foi adicionado mesmo que ele seja excluido esse campo passa a ser dirty
+
+    // O angular atribui algumas classes CSS para que possa ser alterado na folha de estilo CSS
+    ///ng-invalid, ng-valid, 
+    console.log(this.formulario);
+    this.ordemCompraService.efetivaCompra(this.formulario.form.value)
+    .subscribe((idPedido)=>{
+      console.log("o id do pedido é esse:",idPedido)
+      this.idPedidoCompra = idPedido;
+    })
   }
 }
